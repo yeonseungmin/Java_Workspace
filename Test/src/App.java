c6bae3db603c29bb32bd9b70489626f12d668d85
YeonSM <tmdals4544@gnu.ac.kr>
2025-10-17 09:24:58 +0900

    check

--- Test/src/App.java
+++ Test/src/App.java
@@ -1,5 +1,12 @@
+import java.util.Arrays;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);
+        for(int i=0; i<d.length; i++){
+            
+            }
+        return answer;
     }
 }
