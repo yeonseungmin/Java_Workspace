2e3f69800fb107986ab4b22dc449eec551d83f76
YeonSM <tmdals4544@gnu.ac.kr>
2025-10-27 18:56:12 +0900

    Update App2.java

--- p1027_1/src/App2.java
+++ p1027_1/src/App2.java
@@ -3,23 +3,33 @@ public class App2 {
         int N =5;
         int[] stages = {2, 1, 2, 6, 2, 4, 3, 3};
         Solution s1 = new Solution();
-        s1.solution(N,stages);
+        System.out.println(s1.solution(N,stages));
     }
 }
 
 class Solution {
     public int[] solution(int N, int[] stages) {
         int[] answer = {};
-        int count =1;
-        int[] test = new int[N];
+        double totalChellenger;
+        int[] test = new int[N+2];
+        double[] result = new double[stages.length];
+        totalChellenger = test.length;
+
         for(int i =0; i<stages.length; i++){
             test[stages[i]] += 1;  
         }
-        
 
-        for(int e : test){
+        for(int i =0; i<test.length; i++){
+            
+            if(test[i] != 0){
+                result[i]=test[i]/(totalChellenger+1);
+                totalChellenger -= test[i];
+            }
+        }
+        for(double e : result){
             System.out.println(e);
         }
+        
         return answer;
     }
 }
